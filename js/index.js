8a74e865d5618e1514ea238972d0e033c1c3abd3
tadesse123 <birbirsatadesse@gmail.com>
2021-06-09 12:55:31 -0700

    fixing the remove button

--- js/index.js
+++ js/index.js
@@ -34,28 +34,27 @@ skillsList.appendChild(skill)
 
 }
 
-const messageForm = document.getElementsByName("leave_message")[0];
-messageForm.addEventListener("submit", (e) =>{
+let messageForm = document.getElementsByName("leave_message");
+    document.addEventListener("submit", (e) =>{
     e.preventDefault();
     const name = e.target.name.value
     const email = e.target.email.value
     const message = e.target.message.value
     console.log`(Name:${name} Email:${email} Message:${message})`;
     const newMessage = document.createElement("li")
+    const messageSection = document.getElementById("messages")
+    const messageList = messageSection.querySelector("ul")
     newMessage.innerHTML = `<a href="mailto:${email}">${name}</a> wrote: <span>${message}</span> &nbsp;`
-    newMessage.appendChild(removeButton);
-    messageList.appendChild(newMessage);
-    messageForm.reset()
+    const removeButton=document.createElement("button")
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click",(e)=>{
+        const entry = removeButton.parentNode;
+        entry.remove();
 });
-const messageSection = document.getElementById("messages")
-const messageList = messageSection.querySelector("ul")
-
-const removeButton=document.createElement("button")
-removeButton.innerHTML = "remove";
-removeButton.type = "button";
-removeButton.addEventListener("click",(e)=>{
-const entry = e.target.parentNode;
-entry.remove();
+newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    e.target.reset();
 });
 
 
