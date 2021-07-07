3c3f78c5f275798f8e739bfa2a98de48c737f031
tadesse123 <birbirsatadesse@gmail.com>
2021-07-06 17:53:13 -0700

    Changing my website from different page to one page and adding new style

--- js/index.js
+++ js/index.js
@@ -1,38 +1,8 @@
-
-
 let today = new Date();
-//console.log(today);
+// console.log(today);
 
 let thisYear = today.getFullYear();
 //console.log(thisYear);
-
-
-const footer=document.querySelector("footer")
-//console.log(footer)
-
-const copyright = document.createElement("p")
-//console.log(copyright)
-
-copyright.innerHTML=`Tadesse &copy; ${thisYear}`;
-
-footer.appendChild(copyright)
-
-const skills = ['HTML','CSS','Javascript']
-
-const skillsSection=document.getElementById('skills')
-console.log(skillsSection)
-
-const skillsList = skillsSection.querySelector('ul')
-console.log(skills)
-
-for ( let i=0 ; i < skills.length; i++)
-{
-let skill = document.createElement('li')
-skill.innerHTML=skills[i]
-console.log(skill.innerHTML)
-skillsList.appendChild(skill)
-
-}
 
 let messageForm = document.getElementsByName("leave_message");
     document.addEventListener("submit", (e) =>{
@@ -61,6 +31,35 @@ newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
     e.target.reset();
 });
+const footer=document.querySelector("footer")
+//console.log(footer)
+
+const copyright = document.createElement("p")
+//console.log(copyright)
+
+copyright.innerHTML=`Tadesse &copy; ${thisYear}`;
+
+footer.appendChild(copyright)
+
+ const skills = ['HTML','CSS','Javascript']
+
+const skillsSection=document.getElementById('skills')
+console.log(skillsSection)
+
+const skillsList = skillsSection.querySelector('ul')
+console.log(skills)
+
+for ( let i=0 ; i < skills.length; i++)
+{
+let skill = document.createElement('li')
+skill.innerHTML=skills[i]
+console.log(skill.innerHTML)
+skillsList.appendChild(skill)
+
+}
+
+
+
 function myFunction(){
     var x= document.getElementById("myTopnav");
     if (x.className === "topnav"){
@@ -69,26 +68,27 @@ function myFunction(){
         x.className="topnav";
     }
 }
-//  const btn = document.querySelector("#messages")
-//  btn.addEventListener('click', () =>{
-//      if(messageSection.style.display==="none"){
-//          messageSection.style.display="block"
-//      }else{
-//          messageSection.style.display="none";
-//      }
 
-     
-//  });
+const githubRequest = new XMLHttpRequest();
+const url = "https://api.github.com/users/tadesse123/repos";
+githubRequest.open("GET", url, true);
+githubRequest.send();
+githubRequest.onload = function (){
+    let repositories =JSON.parse(this.response);
+    console.log("before parse" , this.respones);
+    console.log("response" , repositories)
+    let projectSection=document.getElementById("project");
+    let projectList=projectSection.querySelector('ul')
+    for ( let i=0; i<repositories.length; i++){
+        let project = document.createElement('li');
+        project.innerHTML=`
+        <a href="${repositories[i].html_url}">${repositories[i].name}</a>
+        <p>${repositories[i].description}</p>
+        `;
+      
+        console.log("my repo", repositories[i].name)
+        projectList.appendChild(project)
+    
 
-
-//document.getElementById("message").style.display="none";
-//document.getElementById("message").style.display="block"
-
-
-
-
-
-
-
-
-
+ }
+ }
